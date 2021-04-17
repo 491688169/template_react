416e3c3aff1189e723688f7daf208219b22c0b66
qianjin <qianjin@learnta.com>
2021-04-17 12:57:49 +0800

    update

--- src/utils/setup/history.tsx
+++ src/utils/setup/history.tsx
@@ -1,11 +1,12 @@
 /*
  * @Author: Kim
  * @Date: 2021-04-15 15:26:50
- * @LastEditTime: 2021-04-16 10:09:24
+ * @LastEditTime: 2021-04-17 12:45:10
  * @LastEditors: Kim
  * @Description:
- * @FilePath: /template_react/src/utils/global/index.ts
+ * @FilePath: /template_react/src/utils/setup/history.tsx
  */
+import { History } from "history";
 import { PATH } from "@/configs/routes.config";
 
 export function gotoSignIn() {
@@ -14,4 +15,11 @@ export function gotoSignIn() {
 
 export function gotoPage(page: PATH, state?: object) {
     G.history.push(page, state);
+}
+
+export function Push(this: History, page: PATH, state?: object, advanced: boolean = true) {
+    if (advanced) {
+        return;
+    }
+    this.push(page, state);
 }
--- src/utils/setup/index.tsx
+++ src/utils/setup/index.tsx
@@ -1,26 +1,25 @@
 /*
  * @Author: Kim
  * @Date: 2021-04-12 09:32:50
- * @LastEditTime: 2021-04-16 10:10:24
+ * @LastEditTime: 2021-04-17 12:53:51
  * @LastEditors: Kim
  * @Description:
  * @FilePath: /template_react/src/utils/setup/index.tsx
  */
-import { History } from "history";
 import { render } from "react-dom";
 import { Router } from "react-router-dom";
-import { createBrowserHistory, createHashHistory } from "history";
-import { ConfigProvider } from "antd";
-import zhCN from "antd/lib/locale-provider/zh_CN";
+import { createBrowserHistory, createHashHistory, History } from "history";
 
 import renderRoutes, { IRoute } from "@/utils/renderRoutes";
-import { gotoSignIn, gotoPage } from "@/utils/global";
+import { gotoSignIn, gotoPage, Push as push } from "./history";
 import ScrollToTop from "@/components/ScrollToTop";
+import React, { useEffect, useState } from "react";
 
 interface IStart {
     routes: IRoute[];
     basename?: string;
     history?: "hash" | "browser";
+    withAntd?: boolean;
 }
 
 interface IG {
@@ -36,18 +35,44 @@ const G: IG = {
 
 window.G = G;
 
-export default function start({ routes, basename, history = "browser" }: IStart) {
-    const historyObj =
+export default function start({ routes, basename, history = "browser", withAntd = false }: IStart) {
+    const customHistory =
         history === "hash" ? createHashHistory({ basename }) : createBrowserHistory({ basename });
-    G.history = historyObj;
+    G.history = customHistory;
+    G.history.push = push;
 
     render(
-        <ConfigProvider locale={zhCN}>
-            <Router history={historyObj}>
+        <AntdWrapper withAntd={withAntd}>
+            <Router history={customHistory}>
                 <ScrollToTop />
                 {renderRoutes(routes)}
             </Router>
-        </ConfigProvider>,
+        </AntdWrapper>,
         document.querySelector("#root")
     );
 }
+
+const AntdWrapper: React.FunctionComponent<{ withAntd: boolean }> = ({ withAntd, children }) => {
+    const [Wrapper, setWrapper] = useState<React.FunctionComponent>(function (props) {
+        return <div {...props}></div>;
+    });
+    const [wrapperProps, setWrapperProps] = useState<object>();
+
+    useEffect(() => {
+        async function renderWrapper() {
+            const [configProvider, zhCN] = await Promise.all([
+                (await import("antd/es/config-provider")).default,
+                (await import("antd/es/locale/zh_CN")).default,
+            ]);
+            setWrapper(configProvider);
+            setWrapperProps({ locale: zhCN });
+        }
+
+        if (withAntd) {
+            renderWrapper();
+        }
+    }, [withAntd]);
+
+    console.log("Wrapper", Wrapper);
+    return <Wrapper {...wrapperProps}>{children}</Wrapper>;
+};
